70a65c2928c0172190f089f9ce0b8ae8d4ce9a1e
Nhân Vượng <nhanvuong2k3@gmail.com>
2022-08-08 08:58:35 +0700

    nothing much

---
+++ scrip.js
@@ -0,0 +1,25 @@
+// alert('lmao')
+// document.write(<h1>test with js</h1>)
+// console.log()
+// //kiểu String
+// var fullName = 'Luong Nhan Vuong'
+// //kiểu number
+// var currentAge = 20
+// //kiểu boolane
+// var male = true
+// //kiểu array
+// var skillAdobe = ['Photoshop','Illustrator','Figma']
+// //kiểu object
+// var personal = {gender: 'Nam', place: 'HaNoi', school: 'Arena'}
+// console.log ('Ho va ten:', fullName)
+// console.log ('Gioi tinh', currentAge)
+// console.log ('Ki nang adobe', skillAdobe )
+// console.log ('Thong tin ca nhan:', personal)
+// var hisAge = 20
+// var herAge = 15
+// console.log('Tuoi cua anh la', hisAge)
+// console.log('Tuoi cua em la', herAge)
+// console.log('Tong so tuoi cua 2 anh em la:', herAge + hisAge)
+// console.log('Tuoi cua anh hon em la:',hisAge - herAge)
+// console.log('Tuoi cua anh nhan tuoi em la:',hisAge * herAge)
+// console.log('Tuoi anh lon hon tuoi em khong',hisAge > herAge)
